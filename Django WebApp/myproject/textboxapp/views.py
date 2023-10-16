ab0c01ad46a335863667f0d2ea59a2783994c8d1
Venkatesh Sridharan <vs498@njit.edu>
2023-10-16 18:57:31 -0400

    Added ML Model to Backend and Templates

--- Django WebApp/myproject/textboxapp/views.py
+++ Django WebApp/myproject/textboxapp/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+import keras
+import pickle
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+
+
+# Load the machine learning model and data
+model = keras.models.load_model('mlfiles/next_word_try_2.keras')
+
+with open('mlfiles/tokenizer.pickle', 'rb') as file:
+    tokenizer = pickle.load(file)
 
 def textbox_page(request):
     return render(request, 'textboxapp/textbox.html')
+
