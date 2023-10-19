437cd2f2e9333abc467ea5bb4f4d4989a6d91390
Venkatesh Sridharan <vs498@njit.edu>
2023-10-19 15:51:54 -0400

    Added '/pretrained' url and setup

--- Django WebApp/myproject/myproject/urls.py
+++ Django WebApp/myproject/myproject/urls.py
@@ -5,5 +5,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('custom/', include('custom_model.urls')),
+    path('pretrained/', include('pretrained_model.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
